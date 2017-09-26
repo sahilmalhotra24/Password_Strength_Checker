acfdc808144101a56267334c704be7efa58b49f9
Sahil Malhotra <sahilmalhotra578@gmail.com>
2017-09-26 23:17:57 +0530

    Update Password_Strength_Checker.py

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -1,3 +1,4 @@
+##this is pasword checker 
 password = raw_input('Enter the password ')
 special = '!@#$%^&*()?'
 if len(password) >= 8 and not password.islower() and not password.isupper() and not password.isalpha() and not password.isdigit() and any((c in special) for c in password):
